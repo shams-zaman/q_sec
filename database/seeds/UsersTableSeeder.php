f98b653fc5a39db9fd326637a4a38cdd7e13d422
alamin <massalamin55@gmail.com>
2021-07-29 13:54:22 +0100

    seed compe

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,21 +2,27 @@
 
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
     public function run()
     {
-        $users = [
-            [
-                'id'             => 1,
-                'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
-                'password'       => '68686868',
-                'remember_token' => null,
-            ],
-        ];
-
-        User::insert($users);
+        DB::table('users')->insert([
+            'id' => '1',
+            'name' => 'Admin',
+            // 'username' => 'admin',
+            'email' => 'a@a.com',
+            'password' => bcrypt('68686868'),
+        ]);
     }
 }
+// $users = [
+//     [
+//         'id'             => 1,
+//         'name'           => 'Admin',
+//         'email'          => 'admin@admin.com',
+//         'password'       => '68686868',
+//         'remember_token' => null,
+//     ],
+// ];
